3ba97a7eafea48dc2ebb043fe06b7d027780504d
Максим Сависько <maksym.savysko@hs-augsburg.de>
2017-12-05 17:06:38 +0200

    Add user routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let url = require('url');
 const Films = require('./app/films');
+const Users = require('./app/users');
 
 const PORT = 3000;
 
@@ -15,6 +16,11 @@ app.put('/api/film', Films.add);
 app.get('/api/film/:id', Films.get);
 app.post('/api/film/:id', Films.update);
 app.delete('/api/film/:id', Films.del);
+
+app.put('/api/user', Users.add);
+app.get('/api/user/:id', Users.get);
+app.post('/api/user/:id', Users.update);
+app.delete('/api/user/:id', Users.del);
 
 app.listen(app.get('port'), function () {
     console.log("Server is running on port " + app.get('port'));
