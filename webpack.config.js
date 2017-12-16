599f9d944276ae90a4b3f8c1b345142096a21dc1
kadmaxim <max.kadatsky@gmail.com>
2017-12-16 20:49:37 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+module.exports = {
+    entry : './public/js/app.js',
+    output : {
+        path : path.resolve(__dirname, './public/dist'),
+        publicPath : './dist/',
+        filename : 'build.js'
+    },
+    module : {
+        rules : [
+            {
+                test : /\.css$/,
+                use : [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test : /\.vue$/,
+                loader : 'vue-loader',
+                options : {
+                    postcss: [require('postcss-cssnext')()]
+                }
+            },
+            {
+                test : /\.js$/,
+                loader : 'babel-loader',
+                exclude : /node_modules/
+            },
+            {
+                test : /\.(png|jpg|gif|svg)$/,
+                loader : 'file-loader',
+                options : {
+                    name : '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve : {
+        alias : {
+            'vue$' : 'vue/dist/vue.esm.js'
+        },
+        extensions : ['*', '.js', '.vue', '.json']
+    },
+    devtool : '#eval-source-map'
+};
